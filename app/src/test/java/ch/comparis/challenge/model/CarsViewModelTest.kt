df23cb1de03ab6a6753884358242f5e39c3ecab5
Anya <ana.gordeev@muume.com>
2022-09-09 13:04:02 +0300

    added tests for cars and filterview models.

--- app/src/test/java/ch/comparis/challenge/model/CarsViewModelTest.kt
+++ app/src/test/java/ch/comparis/challenge/model/CarsViewModelTest.kt
@@ -18,6 +18,7 @@ import org.koin.test.KoinTest
 import org.mockito.Mockito
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
+import kotlin.test.assertNotNull
 
 @RunWith(RobolectricTestRunner::class)
 @Config(application = TestApplication::class)
@@ -44,9 +45,39 @@ internal class CarsViewModelTest : KoinTest {
     }
 
     @Test
-    fun `Check car list is non null` () {
-        assertThat(carsViewModel.cars, notNullValue())
+    fun addToFavoriteTest() {
+        val car = Car(Make(1,"Audi"),"https://tdrresearch.azureedge.net/photos/chrome/Expanded/White/2021AUC280001/2021AUC28000101.jpg", 18000)
+        carsRepository.addCarToFavorite(car.make.name)
+        assertNotNull(carsRepository.addCarToFavorite("Audi"))
+    }
 
+    @Test
+    fun `test if add to favorites`() {
+        val filter = CarsFilter()
+        var cacheCars = carsRepository.fetchCars()
+        if (filter.showFavorite) {
+            cacheCars = cacheCars.filter { it.isFavorite }
+            assertThat(cacheCars, notNullValue())
+        }
+    }
+
+    @Test
+    fun `test cars filter by make`() {
+        val filter = CarsFilter()
+        var cacheCars = carsRepository.fetchCars()
+        if (filter.makes!!.isNotEmpty()) {
+            cacheCars = cacheCars.filter { it.make.isSelected }
+            assertNotNull(cacheCars)
+        }
+    }
+
+    @Test
+    fun `test cars filter by mileage`() {
+        val mileageFrom = 1000
+        val mileageTo = 35600
+        var cacheCars = carsRepository.fetchCars()
+        cacheCars = cacheCars.filter { it.mileage in mileageFrom..mileageTo }
+        assertThat(cacheCars, notNullValue())
     }
 
     @After
--- app/src/test/java/ch/comparis/challenge/model/FiltersViewModelTest.kt
+++ app/src/test/java/ch/comparis/challenge/model/FiltersViewModelTest.kt
@@ -1,25 +1,29 @@
 package ch.comparis.challenge.model
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import ch.comparis.challenge.repo.CarsRepository
 import ch.comparis.challenge.TestApplication
+import ch.comparis.challenge.repo.CarsRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
+import org.hamcrest.CoreMatchers.notNullValue
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.koin.test.KoinTest
-import org.koin.test.inject
+import org.mockito.Mockito.mock
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
+import kotlin.test.assertNotNull
 
 @RunWith(RobolectricTestRunner::class)
 @Config(application = TestApplication::class)
 internal class FiltersViewModelTest : KoinTest {
-    private val carsRepository: CarsRepository by inject()
+     private val carsRepository =  mock(CarsRepository::class.java)
     private lateinit var filtersViewModel: FiltersViewModel
 
     private val mainThreadSurrogate = TestCoroutineDispatcher()
@@ -33,6 +37,18 @@ internal class FiltersViewModelTest : KoinTest {
     fun init() {
         Dispatchers.setMain(mainThreadSurrogate)
         filtersViewModel = FiltersViewModel(carsRepository)
+    }
+
+    @Test
+    fun `test if get all makes`() {
+       val carMakes = carsRepository.fetchCars().map { it.make }.toMutableList()
+        assertThat(carMakes, notNullValue())
+    }
+
+    @Test
+    fun resetFilterTest() {
+        filtersViewModel.updateFilter(CarsFilter())
+        assertNotNull(CarsFilter())
     }
 
 
